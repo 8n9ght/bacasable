28e979c6d0e833b6186f6cfc335be480e986729b
8n9ght <80909968+8n9ght@users.noreply.github.com>
2021-05-04 21:49:11 +0200

    import du projet

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('f-name').addEventListener('input',function(e){
+    document.getElementById('f-name-r').innerHTML = e.target.value;
+});
+
+document.getElementById('l-name').addEventListener('input',function(e){
+    document.getElementById('l-name-r').innerHTML = e.target.value;
+});
+
+document.getElementById('p-number').addEventListener('input',function(e){
+    document.getElementById('p-number-r').innerHTML = e.target.value;
+});
+
+document.getElementById('e-mail').addEventListener('input',function(e){
+    document.getElementById('e-mail-r').innerHTML = e.target.value;
+});
